e4cddbc4616a30b8a137a329715215d410cfa1d8
Jordla <jlscoding@gmail.com>
2024-04-29 15:50:08 -0600

    Add coyote jump feature

--- player.gd
+++ player.gd
@@ -8,8 +8,9 @@ const JUMP_VELOCITY: float = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var animated_sprite_2d = $AnimatedSprite2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 func _physics_process(delta): # Ran everysingle physics frame (60 ticks per second, therefore called 60 times a second) - Delta: Time between each frame 
 	apply_gravity(delta) # try commenting out gravity function to see what happens
@@ -21,33 +22,40 @@ func _physics_process(delta): # Ran everysingle physics frame (60 ticks per seco
 	apply_friction(input_axis, delta)
 	update_animation(input_axis)
 	
+	var was_on_floor = is_on_floor() # Check if player was on floor before moving 
+	
 	
 	# print(delta) - checking what delta doe
 	move_and_slide()
 	
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0 # Before moving: on floor?, After moving: in air/! on_floor, Is player in falling state?
+	if just_left_ledge:
+		coyote_jump_timer.start()
+		
 	
-func apply_gravity(delta):
+	
+func apply_gravity(delta : float):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta # Apply fraction of gravity per tick, delta > 1, then velocity.y is increased to catchup on time gap
 		
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0: # Checks if on floor or coyote jump window is still open, then allow player to jump
 		if Input.is_action_just_pressed("ui_accept"): # Removing is_on_floor() enables endless jumping/flying
 			velocity.y = JUMP_VELOCITY # Jump veloicty applied imm but 
-	else: # In the air 
+	if not is_on_floor(): # In the air 
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2: # Check if not falling and SMALLER than (-200.0), JUMP_VELOCITY - Certain time window to release spacebar for short jump
 			velocity.y = JUMP_VELOCITY / 2 # Short press on spacebar causes a smaller jump
 			
-func apply_friction(input_axis, delta):
+func apply_friction(input_axis : float, delta : float):
 	if input_axis == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	
-func handle_acceleration(input_axis, delta):
+func handle_acceleration(input_axis : float, delta : float):
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
 		
-func update_animation(input_axis):
+func update_animation(input_axis : float):
 	if input_axis != 0:
 		animated_sprite_2d.flip_h = (input_axis < 0)
 		animated_sprite_2d.play("run")
