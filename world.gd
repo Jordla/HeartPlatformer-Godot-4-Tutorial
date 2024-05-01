5ce5eb5cb05d6e419104673917b5ef12e2cc98cc
Jordla <jlscoding@gmail.com>
2024-05-01 17:23:54 -0600

    Replace level polygon structure with tiles

--- world.gd
+++ world.gd
@@ -4,14 +4,14 @@ extends Node2D
 # Static body (This is like a container) 
 	# CollisionPolygon2D is the collision hitbox 
 	# Polygon2D is the actual shape being displayed to screen when the scene runs
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/Polygon2D # Control drag shortcut
+# @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+# @onready var polygon_2d = $StaticBody2D/Polygon2D # Control drag shortcut
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready(): 
 	level_completed.hide()
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	polygon_2d.polygon = collision_polygon_2d.polygon # Set the empty list of polygons to be the list of polygon from the collison node
+	# polygon_2d.polygon = collision_polygon_2d.polygon # Set the empty list of polygons to be the list of polygon from the collison node
 	Events.level_completed.connect(show_level_completed) # Don't put show_level_completed() as we don't want to call the func only connect it
 	
 func show_level_completed():
