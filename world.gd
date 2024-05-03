bb03ba3d596f4384ba34fc3871eeff631eae5890
Jordla <jlscoding@gmail.com>
2024-05-03 14:17:49 -0600

    Fix game infinitely pausing and failing to switch to next level upon level completion

--- world.gd
+++ world.gd
@@ -17,7 +17,7 @@ func _ready():
 func show_level_completed():
 	level_completed.show()
 	get_tree().paused = true # Upon level completion pause the game
-	await get_tree().create_timer(1, 0).timeout # Wait for 1 second timer to be created and then timeout before executing next line of code
+	await get_tree().create_timer(0.5, 1).timeout # Wait for 1 second timer to be created and then timeout before executing next line of code
 	if not next_level is PackedScene: return # Exit clause, What is not next_level? (!PackedScene)?, Exits if PackedScene returns null
 	await LevelTransition.fade_to_black() # await here is waiting for function to finish, await in the function is waiting for animation to finish
 	get_tree().paused = false	# Unpause
