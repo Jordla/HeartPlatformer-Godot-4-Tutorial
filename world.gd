07a7d6f9b682b5a0b5d14ba575717bdd178fa684
Jordla <jlscoding@gmail.com>
2024-05-04 14:20:14 -0600

    Add countdown timer screen before a level starts - Start Tutorial Video P13

--- world.gd
+++ world.gd
@@ -7,12 +7,22 @@ extends Node2D
 # @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
 # @onready var polygon_2d = $StaticBody2D/Polygon2D # Control drag shortcut
 @onready var level_completed = $CanvasLayer/LevelCompleted
+@onready var start_in = %StartIn
+@onready var start_in_label = %StartInLabel
+@onready var animation_player = $AnimationPlayer
 
 
 func _ready(): 
+	get_tree().paused = true
+	LevelTransition.fade_from_black()
+	animation_player.play("countdown")
+	await animation_player.animation_finished
+	get_tree().paused = false
 	# level_completed.hide()
 	# polygon_2d.polygon = collision_polygon_2d.polygon # Set the empty list of polygons to be the list of polygon from the collison node
 	Events.level_completed.connect(show_level_completed) # Don't put show_level_completed() as we don't want to call the func only connect it
+	
+	
 	
 func show_level_completed():
 	level_completed.show()
@@ -22,7 +32,10 @@ func show_level_completed():
 	await LevelTransition.fade_to_black() # await here is waiting for function to finish, await in the function is waiting for animation to finish
 	get_tree().paused = false	# Unpause
 	get_tree().change_scene_to_packed(next_level) # How does it know what the next PackedScene is?
-	LevelTransition.fade_from_black()
+	
 
 	
 	
+
+
+
